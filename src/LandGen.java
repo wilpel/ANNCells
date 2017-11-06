b38898ce25240c9325e55f5924fb8d46967d1637
tiptop96 <gustav@eiman.net>
2017-11-06 11:04:00 +0100

    Proc gen updates(needs seeding)

--- src/LandGen.java
+++ src/LandGen.java
@@ -11,6 +11,7 @@ public class LandGen {
 	public double noise;
 	public Rectangle landPc = new Rectangle(0, 0, 0, 0);
 	public Color green = new Color(0f, 1f, 0f);
+	public Color yellow = new Color(1f, 1f, 0f);
 	public Color blue = new Color(0f, 0f, 1f);
 	public Color white = new Color(1f, 1f, 1f);
 	public Color this_color;
@@ -25,7 +26,9 @@ public class LandGen {
 	public void render(Graphics g) {
 		if (noise < -0.2) {
 			this_color = blue;
-		} else if(noise < 0.7) {
+		} else if(noise < 0.2) {
+			this_color = yellow;
+		} else if(noise < 0.8) {
 			this_color = green;
 		} else {
 			this_color = white;
